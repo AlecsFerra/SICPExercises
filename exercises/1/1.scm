3ff5bad584de19073a4df02a838930eb58b62d7e
alecsferra <aferrarini01@gmail.com>
2019-09-24 14:15:52 +0000

    done ex 1.1

--- exercises/1/1.scm
+++ exercises/1/1.scm
@@ -1,4 +1,5 @@
-;;; Exercise 1.1:Below is a sequence of expressions. What isthe result printed by the interpreter in response to each ex-pression?
+;;; Exercise 1.1:Below is a sequence of expressions.
+;;; What isthe result printed by the interpreter in response to each ex-pression?
 ;;; Assume that the sequence is to be evaluated inthe order in which it is presented.
 
 10 ;10
@@ -19,7 +20,22 @@
 
 (= a b) ; #f
 
-(if (and (> b a) (< b (* a b ))) ; eval as (and (> 4 3) (< 4 (* 3 4))) -> (and #t (< 4 (* 3 4))) ->
+(if (and (> b a) (< b (* a b ))) ; (and (> 4 3) (< 4 (* 3 4))) -> (and #t (< 4 (* 3 4))) ->
                                  ; (and #t (< 4 12)) -> (and #t #t) -> #t
     b  ; since the predicate is #t the b value is returned
     a)
+
+(cond ((= a 4) 6) ; (= a 4) -> (= 3 4) -> #f
+      ((= b 4) (+ 6 7 a)) ; (= b 4) -> (= 4 4) -> #t
+                          ; (+ 6 7 a) -> (+ 6 7 3) -> 16
+                          ; 16 is returned
+      (else 25)
+
+(+ 2 (if (> b a) b a)) ; (> b a) -> (> 4 3) -> #t
+                       ; so the 1st arg is returned b -> 4
+                       ; (+ 2 4) -> 6
+
+(* (cond ((> a b) a) ; (> a b) -> (> 3 4) -> #f
+         ((< a b) b) ; (< a b) -> (< 3 4) -> #t - so is returned b -> 4
+         (else -1))
+    (+ a 1)) ; (* 4 (+ 3 1)) -> (* 4 4) -> 16
